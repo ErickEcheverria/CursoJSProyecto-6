edb06aceb855fadb14ae7d8c7217924e11402363
Erick Echeverría <erickecheverria66@gmail.com>
2021-06-25 02:35:15 -0600

    Cambios realizados

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+// Variables
+let precioBase, valorEuropeo, valorAmericano, valorAsiatico; // Valores para asignar valor y hacer los calculos
+
+const borrarDatosBtn = document.querySelector('#borrarDatos'); // Variable que hace referencia al boton
+
+let datosEmpresa = []; // Arreglo que contendra la informacion de la empresa
+
 // Constructores
 function Seguro(marca,year,tipo){
     this.marca = marca;
@@ -9,17 +16,17 @@ function Seguro(marca,year,tipo){
 Seguro.prototype.cotizarSeguro = function() {
 
     let cantidad;
-    const base = 2000;
+    const base = precioBase;
 
     switch(this.marca){
         case'1':
-            cantidad = base * 1.15;
+            cantidad = base * valorAmericano;
             break;
         case'2':
-            cantidad = base * 1.05;
+            cantidad = base * valorAsiatico;
             break;
         case'3':
-            cantidad = base * 1.35;
+            cantidad = base * valorEuropeo;
             break;
         default:
             break;
@@ -35,6 +42,7 @@ Seguro.prototype.cotizarSeguro = function() {
     }
 
     return cantidad;
+
 }
 
 
@@ -77,6 +85,8 @@ InterfazUsuario.prototype.mostrarMensaje = (mensaje,tipo) => {
 
 InterfazUsuario.prototype.mostrarResultado = (total,seguro) => {
 
+    const resultadoDiv = document.querySelector('#resultado');
+
     const {marca,year,tipo} = seguro;
 
     let textoMarca;
@@ -100,15 +110,12 @@ InterfazUsuario.prototype.mostrarResultado = (total,seguro) => {
     div.classList.add('mt-10');
 
     div.innerHTML = `
-        <p class="header">Tu Resumen</p>
-        <p class="font-bold">Marca: <span class="font-normal">${textoMarca} </span></p>
-        <p class="font-bold">Año: <span class="font-normal">${year} </span></p>
-        <p class="font-bold">Tipo: <span class="font-normal capitalize">${tipo} </span></p>
-        <p class="font-bold">Total <span class="font-normal">$ ${total} </span></p>
+    <p class='header'>Tu Resumen: </p>
+    <p class="font-bold">Marca: <span class="font-normal"> ${textoMarca} </span> </p>
+    <p class="font-bold">Año: <span class="font-normal"> ${year} </span> </p>
+    <p class="font-bold">Tipo: <span class="font-normal"> ${tipo} </span> </p>
+    <p class="font-bold"> Total: <span class="font-normal"> $ ${total} </span> </p>
     `;
-
-    const resultadoDiv = document.querySelector('#resultado');
-    
 
     // Mostramos el spinner
     const spinner = document.querySelector('#cargando');
@@ -121,13 +128,63 @@ InterfazUsuario.prototype.mostrarResultado = (total,seguro) => {
 
 
 }
-
 //Instanciando interfaz de usuario
 const ui = new InterfazUsuario();
 
 document.addEventListener('DOMContentLoaded', () => {
     ui.llenarOpciones(); // llenando listado de años
+
+    datosEmpresa = JSON.parse(localStorage.getItem('datosEmpresa')) || []; // Lee los datos en el LocalStorage
+    
+    if(datosEmpresa.length == 0){
+        cargarDatosEmpresa(); // Si no hay datos, ejecutara la funcion cargarDatosEmpresa
+    }else{
+        asignarValores(); // Si los hay, ejecuta la funcion para asignar los valores
+    }
+    
+    borrarDatosBtn.addEventListener('click', borrarDatos);
 })
+
+function asignarValores(){
+    precioBase = datosEmpresa[0].precioBase; // Solamente almacenaremos un arreglo, entonces los datos estarán en la Pos 0
+    valorEuropeo = datosEmpresa[0].valorEuropeo;
+    valorAsiatico = datosEmpresa[0].valorAsiatico;
+    valorAmenericano = datosEmpresa[0].valorAmericano;
+}
+
+function cargarDatosEmpresa(){
+
+    precioBase = prompt('Defina un Precio base para los seguros'); // Preguntando Datos al usuario para almacenarlos en las variables
+    valorEuropeo = prompt('Defina el % de aumento para los autos Europeos');
+    valorAsiatico = prompt('Defina el % de aumento para los autos Asiaticos');
+    valorAmenericano = prompt('Defina el % de aumento para los autos Americanos');
+
+    const datosObj ={ // Guarda la informacion en un objeto
+        precioBase,
+        valorEuropeo,
+        valorAsiatico,
+        valorAmenericano
+    }
+
+    // Añadir al arreglo de taread
+    datosEmpresa = [...datosEmpresa, datosObj]; // Guardamos el objeto en el arreglo
+
+    sincronizarStorage(); // Sincronizamos en el Localstorage
+}
+
+function sincronizarStorage(){
+    localStorage.setItem('datosEmpresa', JSON.stringify(datosEmpresa)); // Convertimos en String y guardamos
+}
+
+function borrarDatos(){
+    datosEmpresa = []; // Vaciamos el arreglo
+
+    sincronizarStorage();
+
+    location.reload(); // Recargamos la página desde 0
+}
+
+
 
 eventListeners();
 function eventListeners(){
@@ -137,6 +194,8 @@ function eventListeners(){
 
 function cotizarSeguro(e){
     
+    e.preventDefault();
+
     //Leer la marca seleccionada
     const marca = document.querySelector('#marca').value;
 
@@ -151,19 +210,20 @@ function cotizarSeguro(e){
         return;
     }
 
-    ui.mostrarMensaje('Cotizando...','exito');
-
     // Ocultar cotizaciones anteriores
-    const resultado = document.querySelector('#resultado div');
-    if(resultado != null){
-        resultado.remove();
+    const resultados = document.querySelector('#resultado div');
+    if(resultados != null){
+        resultados.remove();
     }
 
     // Instanciar el seguro
      const seguro = new Seguro(marca,year,tipo);
      const total = seguro.cotizarSeguro();
-    
 
     // Utilizar el prototype que contizara
     ui.mostrarResultado(total, seguro);
+    ui.mostrarMensaje('Cotizando...','exito');
+
+    
 }
+
